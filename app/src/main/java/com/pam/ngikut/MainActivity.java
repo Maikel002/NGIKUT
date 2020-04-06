a39f798e8ff7b6a4c1a93716ba1ca51acac03ea9
andreas298 <672017092@student.uksw.edu>
2020-04-06 13:28:25 +0700

    Sign Out Button

--- app/src/main/java/com/pam/ngikut/MainActivity.java
+++ app/src/main/java/com/pam/ngikut/MainActivity.java
@@ -1,80 +1,30 @@
 package com.pam.ngikut;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.pam.ngikut.fragments.HomeFragment;
-import com.pam.ngikut.fragments.ListFragment;
-import com.pam.ngikut.fragments.ProfileFragment;
-import com.pam.ngikut.fragments.SearchFragment;
-public class MainActivity extends AppCompatActivity {
-    Button signOutBtn;
-    BottomNavigationView bottomNavigation;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bottomNavigation = findViewById(R.id.bottom_navigation);
-        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
-        openFragment(HomeFragment.newInstance("", ""));
-        signOutBtn = (Button) findViewById(R.id.signOutButton);
-
-//        signOutBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                FirebaseAuth.getInstance().signOut();
-//                Intent i = new Intent(MainActivity.this, LoginActivity.class);
-//                Toast.makeText(getApplicationContext(), "Signed Out", Toast.LENGTH_SHORT).show();
-//                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-//                startActivity(i);
-//                finish();
-//
-//            }
-//        });
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_profile)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 
-    public void openFragment(Fragment fragment) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.container, fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
-    }
-
-    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    switch (item.getItemId()) {
-                        case R.id.navigation_home:
-                            openFragment(HomeFragment.newInstance("", ""));
-                            return true;
-                        case R.id.navigation_list:
-                            openFragment(ListFragment.newInstance("", ""));
-                            return true;
-                        case R.id.navigation_search:
-                            openFragment(SearchFragment.newInstance("", ""));
-                            return true;
-                        case R.id.navigation_profile:
-                            openFragment(ProfileFragment.newInstance("", ""));
-                            return true;
-                    }
-                    return false;
-                }
-
-
-            };
 }
-
